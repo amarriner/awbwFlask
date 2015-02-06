74fe7bf98ca5c572d2f572177df59721225341e1
Aaron Marriner <amarriner@amarriner.com>
2015-02-06 07:24:05 -0700

    Creating initial (and incomplete) User table

---
+++ resources/alchemy/User.py
@@ -0,0 +1,36 @@
+#!/home1/amarrine/python/bin/python
+
+from sqlalchemy import MetaData, Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from passlib.apps import custom_app_context as pwd_context
+
+import json
+
+Base = declarative_base()
+
+class User(Base):
+   __tablename__ = 'awbw_users'
+
+   id = Column(Integer, primary_key=True)
+   username = Column(String(40), index=True, nullable=False)
+   password_hash = Column(String(128), nullable=False)
+
+   meta = MetaData()
+
+   def __repr__(self):
+      return json.dumps({"User": json.dumps(self.json())})
+
+   def create_table(self, engine):
+      Base.metadata.create_all(engine)
+
+   def json(self):
+      return {
+         "id"              : self.id,
+         "name"            : self.name
+      }
+
+   def hash_password(self, password):
+      self.password_hash = pwd_context.encrypt(password)
+
+   def verify_password(self, password):
+      return pwd_context.verify(password, self.password_hash)
--- resources/alchemy/create_tables.py
+++ resources/alchemy/create_tables.py
@@ -1,9 +1,11 @@
 #!/home1/amarrine/python/bin/python
 
-import AlchemyDB, UnitType
+import AlchemyDB, UnitType, User
 
 adb = AlchemyDB.AlchemyDB()
 
 u = UnitType.UnitType()
 u.create_table(adb.engine)
 
+u = User.User()
+u.create_table(adb.engine)
