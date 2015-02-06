6526a93f0b0ad38ef05b625486e1da72db89e77a
Aaron Marriner <amarriner@amarriner.com>
2015-02-06 10:11:25 -0700

    Forcibly adjusting sys.path, probably a better way

--- api.fcgi
+++ api.fcgi
@@ -3,6 +3,9 @@
 from flup.server.fcgi import WSGIServer
 from api import app
 
+import os,sys
+
+sys.path.append("/home1/amarrine/python/projects/awbwFlask")
  
 if __name__ == '__main__':
    WSGIServer(app).run()
--- api.py
+++ api.py
@@ -1,13 +1,19 @@
 #!/home1/amarrine/python/bin/python
 
+import sys
+
+sys.path.append('/home1/amarrine/python/projects')
+
 from flask import Flask, request
 from flask.ext.restful import abort, Api, Resource, reqparse
 
-from resources.UnitTypeAPI import UnitType_EP
-from resources.UnitTypeAPI import UnitType_ID_EP
+from awbwFlask.resources.UnitTypeAPI import UnitType_EP
+from awbwFlask.resources.UnitTypeAPI import UnitType_ID_EP
 
-from resources.UserAPI import User_EP
-from resources.UserAPI import User_ID_EP
+from awbwFlask.resources.UserAPI import User_EP
+from awbwFlask.resources.UserAPI import User_ID_EP
+
+from awbwFlask.resources.LoginAPI import Login_EP
 
 app = Flask(__name__)
 api = Api(app)
@@ -17,6 +23,8 @@ api.add_resource(UnitType_ID_EP, '/unit-type/<int:id>')
 
 api.add_resource(User_EP, '/user')
 api.add_resource(User_ID_EP, '/user/<int:id>')
+
+api.add_resource(Login_EP, '/login',)
 
 if __name__ == '__main__':
    app.run(debug=True)
