22afc1d8abcaacc49d13b99a2303a0a696eea518
amarriner <amarriner@amarriner.com>
2015-02-13 13:59:30 -0500

    Removing test files

--- api.py
+++ api.py
@@ -11,8 +11,6 @@ from awbwFlask.resources.UserAPI import User_EP, User_ID_EP
 from awbwFlask.resources.TerrainTypeAPI import TerrainType_EP, TerrainType_ID_EP
 from awbwFlask.resources.LoginAPI import Login_EP
 
-from awbwFlask.resources.TestData import TestData_EP, TestData_ID_EP
-
 api.add_resource(CountryType_EP, '/api/country-type')
 api.add_resource(CountryType_ID_EP, '/api/country-type/<string:id>')
 
@@ -26,9 +24,6 @@ api.add_resource(TerrainType_EP, '/api/terrain-type')
 api.add_resource(TerrainType_ID_EP, '/api/terrain-type/<int:id>')
 
 api.add_resource(Login_EP, '/api/login')
-
-api.add_resource(TestData_EP, '/api/test-data')
-api.add_resource(TestData_ID_EP, '/api/test-data/<string:id>')
 
 if __name__ == '__main__':
    app.run(debug=True)
